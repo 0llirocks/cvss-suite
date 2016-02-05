db91395e3146bd1f65879bf4289d0b8189ab8aa5
Oliver Hamboerger <oliver.hamboerger@siemens.com>
2016-02-05 18:05:38 +0100

    Cleared directory

--- lib/cvss/suite/gem.rb
+++
@@ -1,9 +0,0 @@
-require "cvss/suite/gem/version"
-
-module Cvss
-  module Suite
-    module Gem
-      # Your code goes here...
-    end
-  end
-end
--- lib/cvss/suite/gem/version.rb
+++
@@ -1,7 +0,0 @@
-module Cvss
-  module Suite
-    module Gem
-      VERSION = "0.1.0"
-    end
-  end
-end
