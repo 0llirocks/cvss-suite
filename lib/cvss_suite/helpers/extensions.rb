d22c9a8871633e11d03100b1b7081766febe7622
Oliver Hamboerger <oliver.hamboerger@siemens.com>
2016-02-10 13:30:44 +0100

    Added round_up method for float

--- lib/cvss_suite/helpers/extensions.rb
+++ lib/cvss_suite/helpers/extensions.rb
@@ -1,5 +1,5 @@
 class Float
-  def round_up(decimal_paces)
+  def round_up(decimal_paces = 0)
     (self*10.0**decimal_paces).ceil/10.0**decimal_paces
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cvss_suite'
+require 'cvss_suite/helpers/extensions'
 require 'rspec/its'
