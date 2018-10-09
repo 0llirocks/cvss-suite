d2198e63743a46ffdc017a6c4001609cbc1d06e9
Oliver Hamboerger <oliver.hamboerger@siemens.com>
2018-10-09 13:07:58 +0200

    Add copyright note

--- lib/cvss_suite/errors.rb
+++ lib/cvss_suite/errors.rb
@@ -1,3 +1,13 @@
+# CVSS-Suite, a Ruby gem to manage the CVSS vector
+#
+# Copyright (c) Siemens AG, 2016
+#
+# Authors:
+#   Adam David <adamrdavid@gmail.com>
+#
+# This work is licensed under the terms of the MIT license.
+# See the LICENSE.md file in the top-level directory.
+
 module CvssSuite
   ##
   # This will define classed errors to be expected
