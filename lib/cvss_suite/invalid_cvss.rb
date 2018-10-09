c096cd8458d9f11d7b303bdc9c635d52560f35ff
Oliver Hamboerger <oliver.hamboerger@siemens.com>
2018-10-09 16:43:48 +0200

    Add new class for invalid vectors

---
+++ lib/cvss_suite/invalid_cvss.rb
@@ -0,0 +1,57 @@
+# CVSS-Suite, a Ruby gem to manage the CVSS vector
+#
+# Copyright (c) Siemens AG, 2018
+#
+# Authors:
+#   Oliver Hambörger <oliver.hamboerger@siemens.com>
+#
+# This work is licensed under the terms of the MIT license.
+# See the LICENSE.md file in the top-level directory.
+
+# ##
+# # This class represents a invalid CVSS vector.
+
+class InvalidCvss < Cvss
+
+  ##
+  # Creates a new invalid CVSS vector.
+
+  def initialize
+  end
+
+  ##
+  # Since this is an invalid CVSS vector, it always returns false.
+
+  def valid?
+    false
+  end
+
+  ##
+  # Since this is an invalid CVSS vector, it always throws an exception.
+
+  def version
+    check_validity
+  end
+
+  ##
+  # Since this is an invalid CVSS vector, it always throws an exception.
+
+  def base_score
+    check_validity
+  end
+
+  ##
+  # Since this is an invalid CVSS vector, it always throws an exception.
+
+  def temporal_score
+    check_validity
+  end
+
+  ##
+  # Since this is an invalid CVSS vector, it always throws an exception.
+
+  def environmental_score
+    check_validity
+  end
+
+end
