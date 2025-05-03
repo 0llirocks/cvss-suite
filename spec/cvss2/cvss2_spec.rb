31290d6a62d1adc6f5d39c1abff6b6ed4e498356
0llirocks <oliver.hamboerger@gmail.com>
2025-05-03 09:51:11 +0200

    Fix rubocop

--- spec/cvss2/cvss2_spec.rb
+++ spec/cvss2/cvss2_spec.rb
@@ -7,7 +7,7 @@ require_relative '../spec_helper'
 
 describe CvssSuite::Cvss2 do
   let(:valid_cvss2) { CvssSuite.new('AV:N/AC:L/Au:N/C:P/I:P/A:P') }
-  let(:valid_cvss2_issue_49) { CvssSuite.new('AV:L/AC:L/Au:N/C:C/I:C/A:C') }
+  let(:valid_cvss2_issue49) { CvssSuite.new('AV:L/AC:L/Au:N/C:C/I:C/A:C') }
   let(:valid_cvss2_parenthesis) { CvssSuite.new('(AV:N/AC:L/Au:N/C:P/I:P/A:P)') }
   let(:valid_cvss2_temporal) { CvssSuite.new('AV:N/AC:L/Au:N/C:P/I:P/A:P/E:U/RL:OF/RC:C') }
   let(:valid_cvss2_temporal_parenthesis) { CvssSuite.new('(AV:N/AC:L/Au:N/C:P/I:P/A:P/E:U/RL:OF/RC:C)') }
@@ -43,7 +43,7 @@ describe CvssSuite::Cvss2 do
   end
 
   describe 'valid cvss2 for issue 49' do
-    subject { valid_cvss2_issue_49 }
+    subject { valid_cvss2_issue49 }
 
     it_behaves_like 'a valid cvss vector', 2, 7.2, 10.0, 3.9, 7.2, 7.2, 7.2, 'High'
   end
